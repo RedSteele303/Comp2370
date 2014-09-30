19c759955d18ed4bd37ba8c58ac6207299e6f345
RedSteele303 <sean.steele@du.edu>
2014-09-29 21:40:09 -0600

    First Round of Finished Product without a check on mergesort

--- src/Assignment1.java
+++ src/Assignment1.java
@@ -17,6 +17,10 @@ public class Assignment1 {
 	public static int maxSize;
 	public static double averageCopyTime, averageInsertionSortTime;
 	public static double averageMergeSortTime;
+	private static double averagePreSortedMergeSortTime;
+	private static double averagePreSortedInsertionSortTime;
+	private static double averageReverseOrderMergeSortTime;
+	private static double averageReverseOrderInsertionSortTime;
 	/**
 	 * Program main
 	 * 
@@ -27,7 +31,7 @@ public class Assignment1 {
 		maxSize = Integer.parseInt(args[0]);
 		//	System.out.println(maxSize);
 
-		for (long n = minSize; n<maxSize; n = n*10){
+		for (long n = minSize; n<=maxSize; n = n*10){
 			//Part 1: Part 1 and 3
 			ArrayList<Double> a = Sort.fillRandomArray(n);
 			//Part 1: Part 2
@@ -68,7 +72,68 @@ public class Assignment1 {
 				System.out.println("One of the array lists was not sorted properly.");
 				System.exit(1);
 			}
-			System.out.println("Average Merge Sort Time: " + averageMergeSortTime + "\n");
+			System.out.println("Average Merge Sort Time: " + averageMergeSortTime);
+			
+			//Part 2: Part 1: Insertion Sort
+			ArrayList<Double> preSorted = new ArrayList<Double>();
+			for(Double i = 0.0; i < n; i++){
+				preSorted.add(i);
+			}
+			CpuTimer timer4 = new CpuTimer();
+			for(int i = 0; i<timingIterations; i++){
+				b = Sort.insertionSort(preSorted);
+			}
+			averagePreSortedInsertionSortTime = (timer4.getElapsedCpuTime()/timingIterations)-averageCopyTime;
+			if(!Sort.isSorted(b)){
+				System.out.println("One of the array lists was not sorted properly.");
+				System.exit(1);
+			}
+			
+			System.out.println("Average Insertion Sort Time for Presorted: " + averagePreSortedInsertionSortTime);
+			
+			//Part 2: Part 1: Merge Sort
+			CpuTimer timer5 = new CpuTimer();
+			for(int i = 0; i<timingIterations; i++){
+				b = Sort.mergeSort(preSorted);
+			}
+			averagePreSortedMergeSortTime = (timer5.getElapsedCpuTime()/timingIterations)-averageCopyTime;
+			if(!Sort.isSorted(b)){
+				System.out.println("One of the array lists was not sorted properly.");
+				System.exit(1);
+			}
+			
+			System.out.println("Average Merge Sort Time for Presorted: " + averagePreSortedMergeSortTime);
+			
+			//Part 2: Part 2: Insertion Sort
+			ArrayList<Double> reverseOrder = new ArrayList<Double>();
+			for(int i = (int) n; i > 0; i--){
+				reverseOrder.add((double) i);
+			}
+			CpuTimer timer6 = new CpuTimer();
+			for(int i = 0; i<timingIterations; i++){
+				b = Sort.insertionSort(reverseOrder);
+			}
+			averageReverseOrderInsertionSortTime = (timer6.getElapsedCpuTime()/timingIterations)-averageCopyTime;
+			if(!Sort.isSorted(b)){
+				System.out.println("One of the array lists was not sorted properly.");
+				System.exit(1);
+			}
+			
+			System.out.println("Average Insertion Sort Time for Reverse Order : " + averageReverseOrderInsertionSortTime);
+			
+			//Part 2: Part 2: Merge Sort
+			CpuTimer timer7 = new CpuTimer();
+			for(int i = 0; i<timingIterations; i++){
+				b = Sort.mergeSort(reverseOrder);
+			}
+			averageReverseOrderMergeSortTime = (timer7.getElapsedCpuTime()/timingIterations)-averageCopyTime;
+			if(!Sort.isSorted(b)){
+				System.out.println("One of the array lists was not sorted properly.");
+				System.exit(1);
+			}
+			
+			System.out.println("Average Merge Sort Time for Reverse Order : " + averageReverseOrderMergeSortTime + "\n");
+			
 
 		}
 		System.out.println("Testing complete.");
